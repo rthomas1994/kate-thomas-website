610708ee0ff318907fc9a7770e0091aa0f63aff1
Rhys Thomas <rhysthomas1994@gmail.com>
2020-05-27 18:05:19 -0400

    Changes

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var scrollOffset = 70;
+
+$(document).ready(function() {
+
+	$("#nav_home").click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 400)
+	});
+
+	$("#nav_work").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#work").offset().top - scrollOffset
+        }, 400)
+	});
+
+    $("#nav_resume").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#resume").offset().top - scrollOffset
+        }, 400)
+    });
+
+	$("#nav_contact").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#contact").offset().top - scrollOffset
+        }, 400)
+	});
+
+});
